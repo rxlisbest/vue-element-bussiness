06e208596c61e248128373ba78fd9868ca19f5e1
Roy <rxlisbest@gmail.com>
2019-01-05 14:18:47 +0800

    element

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import router from './router'
 import App from './App.vue'
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Element, { size: 'small', zIndex: 3000 })
 
 Vue.config.productionTip = false
 
